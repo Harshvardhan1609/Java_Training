b4f71fb1b056415eb5df2447ae8614706efc232d
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2023-02-04 12:38:51 +0530

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int a = 12;
+        int b;
+        System.out.println("Please a number : ");
+        Scanner scs = new Scanner(System.in);
+        b = scs.nextInt();
+        System.out.println("Hello MOTO" + Integer.toString(b));
     }
 }
